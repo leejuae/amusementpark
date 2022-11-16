7759b50fa9946a1e988d53e76df95c2ed691dcb7
leejuae <joojuae02@gachon.ac.kr>
2022-11-17 02:05:06 +0900

    주석 정리 및 main함수 구조 수정 내용면에선 달라진거 없음!

--- src/main.c
+++ src/main.c
@@ -8,6 +8,8 @@
 
 #define MAX 11	//10(놀이기구의 개수) + 1(시작위치)
 
+/*구조체들!*/
+
 //놀이기구의 위치
 typedef struct location {
 	double x;			//x축 위치
@@ -23,6 +25,10 @@ typedef struct ROUTE {
 	bool possible;		//i번째 놀이기구를 방문했는지 방문하지 않았는지 참거짓으로 저장
 }route;
 
+
+
+
+/*전역변수들*/
 loca attractionInfo[MAX];	//놀이기구들의 위치 info[0]은 시작위치에 대한 값임.
 route routeInfo[MAX];	//최소거리로 i번째 놀이기구에 방문하는 방법의 놀이기구 방문순서와 해당 node까지 드는 거리 계산한 것 저장
 double edge[MAX][MAX];	//edge값 저장 2차원 배열
@@ -30,10 +36,19 @@ int order[MAX];	//방문한 놀이기구들을 순서대로 저장함.
 int cnt = 0;	//0이면 거리정보랑 인기도를 처음에 랜덤으로 초기화해줌. 1 이상이면 인기도만 랜덤으로 바뀜
 				// + 현재 위치 기준으로 방문 놀이기구 업데이트 할 때도 사용
 
+
+
+
+/*함수들*/
 void calEdge();	//위치 기준으로 거리 계산하는 함수
 int minDistance();	//최소거리 계산함수
-void printSolution();
+void printAttractionRoute();
 void dijkstra(int);	//다익스트라 알고리즘 수행
+
+
+
+
+
 
 void calEdge() {
 
@@ -64,7 +79,17 @@ void calEdge() {
 			}
 		}
 	}
+
+	else {
+		printf("calEdge함수에서 문제생김");
+	}
 }
+
+
+
+
+
+
 
 // shortest path tree에 아직 포함되지 않은 놀이기구들 중 가장 거리가 가까운 놀이기구를 찾는 함수
 int minDistance()
@@ -82,16 +107,24 @@ int minDistance()
 	return min_index;
 }
 
+
+
+
+
 // 결과값 출력 함수
-void printSolution()
+void printAttractionRoute()
 {
-	int i = 0;
-	printf("visit: ");
-	for (i = cnt; i < MAX; i++) {
+	printf("%d-th visit: ", cnt);
+	for (int i = cnt; i < MAX; i++) {
 		printf("-> %d", order[i]);
 	}
 	printf("\n\n");
 }
+
+
+
+
+
 
 void dijkstra(int src)
 {
@@ -127,42 +160,48 @@ void dijkstra(int src)
 				&& routeInfo[u].distance + edge[u][v] < routeInfo[v].distance)
 				routeInfo[v].distance = routeInfo[u].distance + edge[u][v];
 	}
-
-	// print the constructed distance array
-	printSolution();
 }
 
-int main() {
-	calEdge();
-	dijkstra(0);
 
-	while (1) {
-		srand(time(NULL));
+
+
+
+
+int main() {
+
+	srand(time(NULL));	//프로그램 실행시마다 다른값 나오게 해줌.
+
+	do {
+		//1입력하면 프로그램 실행
 		int num;
-		//1입력하면 다시 프로그램 실행
 		scanf("%d", &num);
 
 		if (num == 1 && cnt < 10) {
-			attractionInfo[order[cnt]].visit = 1;
-			cnt++;
-			//다시 랜덤으로 돌리고 다익스트라 알고리즘 처음부터 돌리는 코드임! 테이블 업데이트하는 코드 아님!!!!!!!!!
 			calEdge();
 			dijkstra(order[cnt]);	//cnt번째 놀이기구를 시작으로 다시 거리계산해 다음 놀이기구 정함.
+			printAttractionRoute();
+			attractionInfo[order[cnt]].visit = 1;
+			cnt++;
 		}
+
+
 		//0입력하면 그냥 종료
 		else if (num == 0 && cnt < 10) {
 			exit(0);
 		}
 
+
+		//cnt==10 되면 놀이기구 다 탄걸로 간주하고 종료
 		else if (cnt >= 10) {
 			printf("놀이기구 다탔어용~!~!~!");
 			exit(0);
 		}
-		
+
+
 		else {
 			printf("문제생긴듯,,,ㅜㅜ");
 		}
-	}
+	} while (1);
 
 	return 0;
 }
